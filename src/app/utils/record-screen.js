153ebda115bf359c0684111741e47a6013cce2a3
kirankn8 <kirankn8@gmail.com>
2018-11-17 14:18:21 +0530

    Fix screen recorder

--- src/app/utils/record-screen.js
+++ src/app/utils/record-screen.js
@@ -14,80 +14,89 @@ export function startRecording() {
         if (error) throw console.log(error.message);
         for (let i = 0; i < sources.length; ++i) {
             console.log(sources);
-            if (sources[i].name === 'Electron') {
+            if (sources[i].name === 'Entire screen' || sources[i].name === 'Screen 1') {
                 navigator.mediaDevices.getUserMedia({
                     audio: false,
                     video: {
-                        mandatory: thumbSize
+                        mandatory: {
+                            chromeMediaSource: 'desktop',
+                            chromeMediaSourceId: sources[i].id,
+                            minWidth: 1280,
+                            maxWidth: 1280,
+                            minHeight: 720,
+                            maxHeight: 720
+                        }
                     }
                 })
                     .then((stream) => handleStream(stream))
-                    .catch((e) => handleError(e))
-                return
+                    .catch((e) => handleError(e));
+                return;
             }
         }
     });
+
+    function determineScreenshot() {
+        const screenSize = electronScreen.getPrimaryDisplay().workAreaSize;
+        const maxDimension = Math.max(screenSize.width, screenSize.height);
+        return {
+            width: maxDimension * window.devicePixelRatio,
+            height: maxDimension * window.devicePixelRatio,
+        }
+    }
+
+
+    function handleError(e) {
+        console.log(e)
+    }
+
+    function handleStream(stream) {
+        recorder = new MediaRecorder(stream);
+        blobs = [];
+        recorder.ondataavailable = function (event) {
+            blobs.push(event.data);
+        };
+        recorder.start();
+    }
 }
 
 
-function handleError(e) {
-    console.log(e)
+export function pauseRecording() {
+    recorder.stop();
 }
 
-function handleStream(stream) {
-    // https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
-    recorder = new MediaRecorder(stream);
-    blobs = [];
-    recorder.ondataavailable = function (event) {
-        blobs.push(event.data);
-    };
-    recorder.start();
-    console.log(recorder);
-}
-
-// TODO: Fix stop Recording function
 export function stopRecording() {
-    console.log(recorder);
+    const saveFile = () => {
+        // covert browser's array buffer to nodejs buffer
+        toArrayBuffer(new Blob(blobs, { type: 'video/webm' }), function (ab) {
+            var buffer = toBuffer(ab);
+            var file = `C:/Users/KIRAN KN/Desktop/example.webm`;
+            fs.writeFile(file, buffer, function (err) {
+                if (err) {
+                    console.error('Failed to save video ' + err);
+                } else {
+                    console.log('Saved video: ' + file);
+                }
+            });
+        });
+    }
+    recorder.onstop = saveFile;
     recorder.stop();
 
-    // https://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer/12101012#12101012
-    toArrayBuffer(new Blob(blobs, { type: 'video/webm' }), function (ab) {
-        var buffer = toBuffer(ab);
-        var file = `./videos/example.webm`;
-        fs.writeFile(file, buffer, function (err) {
-            if (err) {
-                console.error('Failed to save video ' + err);
-            } else {
-                console.log('Saved video: ' + file);
-            }
-        });
-    });
-}
-
-function toArrayBuffer(blob, cb) {
-    let fileReader = new FileReader();
-    fileReader.onload = function () {
-        let arrayBuffer = this.result;
-        cb(arrayBuffer);
-    };
-    fileReader.readAsArrayBuffer(blob);
-}
-
-function toBuffer(ab) {
-    let buffer = new Buffer(ab.byteLength);
-    let arr = new Uint8Array(ab);
-    for (let i = 0; i < arr.byteLength; i++) {
-        buffer[i] = arr[i];
+    function toArrayBuffer(blob, cb) {
+        let fileReader = new FileReader();
+        fileReader.onload = function () {
+            let arrayBuffer = this.result;
+            cb(arrayBuffer);
+        };
+        fileReader.readAsArrayBuffer(blob);
     }
-    return buffer;
-}
 
-
-function determineScreenshot() {
-    const screenSize = electronScreen.getPrimaryDisplay().workAreaSize;
-    const maxDimension = Math.max(screenSize.width, screenSize.height);
-    return {
-        width: maxDimension * window.devicePixelRatio,
-        height: maxDimension * window.devicePixelRatio,
+    function toBuffer(ab) {
+        let buffer = new Buffer(ab.byteLength);
+        let arr = new Uint8Array(ab);
+        for (let i = 0; i < arr.byteLength; i++) {
+            buffer[i] = arr[i];
+        }
+        return buffer;
     }
 }
